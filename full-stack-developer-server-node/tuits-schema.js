ea3ea891ac4bfcb4831eaaf8dc08fb59c8bfe214
Karen Zhu <68167276+zkaren01@users.noreply.github.com>
2022-04-14 20:32:13 -0400

    Delete full-stack-developer-server-node directory

--- full-stack-developer-server-node/tuits-schema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const schema = mongoose.Schema({
-  tuit: String,
-  likes: Number,
-  postedBy: {
-    username: String
-  }
-}, {collection: 'tuits'});
-export default schema;
